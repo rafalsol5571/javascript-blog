9f56ef367a3affdcbc8addd024b02c8550e3e6ad
Rafał Dymek <rafal.dymek93@gmail.com>
2022-07-18 11:32:51 +0200

    blog

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
-  event.preventDefault();
+  event.preventDefault();  //blokowanie zmiany linkow
   const clickedElement = this;
   console.log('Link was clicked!');
+  console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -15,25 +16,38 @@ function titleClickHandler(event){
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+ 
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.titles p.active');
+  const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);   
+ 
+
+  
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+  
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+
+  
 }
