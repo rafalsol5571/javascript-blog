92e8f8b13b49de9ff89c1356e95f39ba0ba245f5
Rafał Dymek <rafal.dymek93@gmail.com>
2022-07-18 17:49:13 +0200

    blog

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
+
 function titleClickHandler(event){
   event.preventDefault();  //blokowanie zmiany linkow
   const clickedElement = this;
@@ -42,9 +46,49 @@ function titleClickHandler(event){
   
   targetArticle.classList.add('active');
 
+  function generateTitleLinks(customSelector = ''){
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+  
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector + customSelector);
+    console.log('articles', articles)
+    let html = '';
+
+    for (let article of articles) {
+  
+      /* get the article id */
+
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+  
+      /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  
+      /* get the title from the title element */
+  
+      /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+  
+      /* insert link into titleList */
+
+      html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+  
+  }
+  
+ 
+
 }
 
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
